9dc370bf86e917478e98d0ffea035ab65b2d8f42
Matthew Petelle <matthew.petelle@gmail.com>
2025-07-18 15:56:24 +0200

    commit 1

---
+++ HT2.1Pred.R
@@ -0,0 +1,7 @@
+################################################
+################################################
+###Code for predictability estimates of HT2.1###
+
+
+##Need to think about sampling for PCA between and within lines##
+
